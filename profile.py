69e4f795c8e917b2d16675a89cefede7bcb9aae2
ghostdiv3 <gcook@protonmail.com>
2019-08-29 22:21:28 -0400

    adding profile.py

---
+++ profile.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import time
+import configparser
+
+###########################################################################
+## This section contains the token-related API calls
+###########################################################################
+
+def token_check():
+    PARSER = configparser.ConfigParser()
+    PARSER.read('.env')
+    CLIENT_ID = PARSER.get('creds', 'CLIENT_ID')
+    CLIENT_SECRET = PARSER.get('creds', 'CLIENT_SECRET')
+    CURR_TOKEN = PARSER.get('tokens', 'TOKEN')
+
+    if CURR_TOKEN == '':
+        print("No token set. Generating new token...\n")
+        time.sleep(1)
+        NEW_TOKEN = token_gen(CLIENT_ID, CLIENT_SECRET)
+        PARSER.set('tokens', 'TOKEN', NEW_TOKEN)
+        ENVFILE = open('.env', 'w')
+        PARSER.write(ENVFILE)
+        ENVFILE.close()
+        return True
+
+    else:
+        print("\nToken already exists. Continuing program...\n")
+
+def token_gen(CLIENTID, CLIENTSECRET):
+    TOKEN_REQ = {'grant_type':'client_credentials', 'scope':'wow.profile'}
+    TOKEN_DATA = requests.post('https://us.battle.net/oauth/token',
+     auth=(CLIENTID, CLIENTSECRET), data=TOKEN_REQ).json()
+    print("Configuring new token...")
+    time.sleep(1)
+    print("Done\n")
+    print(f"New session token    : {TOKEN_DATA['access_token']}")
+    print(f"Token type           : {TOKEN_DATA['token_type']}")
+    print(f"Token time remaining : {TOKEN_DATA['expires_in']}")
+    print(f"Scope of token       : {TOKEN_DATA['scope']}")
+
+    return TOKEN_DATA['access_token']
+
+###########################################################################
+###########################################################################
+
+token_check()
