da859cc2309ed5df93f19730fa15299b7aade4db
ghostdiv3 <gcook@protonmail.com>
2019-09-11 22:28:03 -0400

    added headers for each section

--- gamedata.py
+++ gamedata.py
@@ -14,13 +14,13 @@ AUTH_TOKEN = PARSER.get('tokens', 'TOKEN')
 HEADER_BEARER = f"Bearer {AUTH_TOKEN}"
 AUTH_HEADER = {'Authorization':HEADER_BEARER}
 
-###########################################################################
-## This section sets up the token variable for use in the API calls
-###########################################################################
-
 ## specifying the global variable for the API FQDN
 
 URL = "https://us.api.blizzard.com"
+
+###########################################################################
+## Achivement API
+###########################################################################
 
 def achievement_categories_index():
 
@@ -31,9 +31,12 @@ def achievement_categories_index():
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(RESULT)
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
 
-def achievement_categories(ACHIEVE_CATEGORY_ID):
+    return True
+
+def achievement_category(ACHIEVE_CATEGORY_ID):
 
     API_HTTP_PATH = f"/data/wow/achievement-category/{ACHIEVE_CATEGORY_ID}"
     REGION = "us"
@@ -42,9 +45,12 @@ def achievement_categories(ACHIEVE_CATEGORY_ID):
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(RESULT)
 
-def achievement_index():
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+def achievements_index():
 
     API_HTTP_PATH = f"/data/wow/achievement/index"
     REGION = "us"
@@ -53,9 +59,12 @@ def achievement_index():
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(RESULT)
 
-def achievement_id(ACHIEVE_ID):
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+def achievement(ACHIEVE_ID):
 
     API_HTTP_PATH = f"/data/wow/achievement/{ACHIEVE_ID}"
     REGION = "us"
@@ -64,7 +73,10 @@ def achievement_id(ACHIEVE_ID):
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(RESULT)
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
 
 def achievement_media(ACHIEVE_ID):
 
@@ -75,23 +87,94 @@ def achievement_media(ACHIEVE_ID):
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(RESULT)
-
-
-def creature_info(CREATURE_ID):
     
-    API_HTTP_PATH = f"/data/wow/creature/{CREATURE_ID}"
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+###########################################################################
+## Azerite Essence API
+###########################################################################
+
+def azerite_essences_index():
+
+    API_HTTP_PATH = f"/data/wow/azerite-essence/index"
     REGION = "us"
     NAMESPACE = "static-us"
     LOCALE = "en_US"
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(json.dumps(RESULT, sort_keys=True, indent=4, separators=(',', ': ')))
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True 
+
+def azerite_essence(ESSENCE_ID):
+
+    API_HTTP_PATH = f"/data/wow/azerite-essence/{ESSENCE_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
     
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
     return True
 
-def creature_families():
+def azerite_essence_media(ESSENCE_ID):
+
+    API_HTTP_PATH = f"/data/wow/media/azerite-essence/{ESSENCE_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+###########################################################################
+## Connected Realm API
+###########################################################################
+
+def connected_realms_index():
+
+    API_HTTP_PATH = f"/data/wow/connected-realm/index"
+    REGION = "us"
+    NAMESPACE = "dynamic-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+def connected_realm(REALM_ID):
+
+    API_HTTP_PATH = f"/data/wow/connected-realm/{REALM_ID}"
+    REGION = "us"
+    NAMESPACE = "dynamic-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+
+    return True
+
+###########################################################################
+## Creature API
+###########################################################################
+
+def creature_families_index():
     
     API_HTTP_PATH = f"/data/wow/creature-family/index"
     REGION = "us"
@@ -100,25 +183,106 @@ def creature_families():
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    
-    for i in range(len(RESULT['creature_families'])):
-        print(f"""\nCreature ID: {RESULT['creature_families'][i]['id']}
-                  \nCreature Name: {RESULT['creature_families'][i]['name']}""")
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
     
     return True
 
-def creature_types():
-    
-    API_HTTP_PATH = "/data/wow/creature-type/index"
+def creature_family(CREATURE_ID):
+
+    API_HTTP_PATH = f"/data/wow/creature-family/{CREATURE_ID}"
     REGION = "us"
     NAMESPACE = "static-us"
     LOCALE = "en_US"
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(json.dumps(RESULT, sort_keys=True, indent=4, separators=(',', ': ')))
-
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
     return True
+
+def creature_types_index():
+
+    API_HTTP_PATH = f"/data/wow/creature-type/index"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
+    return True
+
+def creature_types(TYPE_ID):
+
+    API_HTTP_PATH = f"/data/wow/creature-type/{TYPE_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
+    return True
+
+def creature(CREATURE_ID):
+    
+    API_HTTP_PATH = f"/data/wow/creature/{CREATURE_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
+    return True
+
+def creature_display_media(DISPLAY_ID):
+    
+    API_HTTP_PATH = f"/data/wow/media/creature-display/{DISPLAY_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
+    return True
+
+def creature_family_media(FAMILY_ID):
+    
+    API_HTTP_PATH = f"/data/wow/media/creature-family/{FAMILY_ID}"
+    REGION = "us"
+    NAMESPACE = "static-us"
+    LOCALE = "en_US"
+    PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
+    FULL_URL = f"{URL}{API_HTTP_PATH}"
+    RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
+    
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
+    
+    return True
+
+###########################################################################
+## Guild API
+###########################################################################
+
+###########################################################################
+## Guild Crest API
+###########################################################################
+
+###########################################################################
+## Item API
+###########################################################################
 
 def item_classes_index():
     
@@ -129,7 +293,7 @@ def item_classes_index():
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(json.dumps(RESULT, sort_keys=True, indent=4, separators=(',', ': ')))
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
 
     return True
 
@@ -142,7 +306,7 @@ def item_class(ITEM_CLASS_ID):
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(json.dumps(RESULT, sort_keys=True, indent=4, separators=(',', ': ')))
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
 
     return True
 
@@ -174,6 +338,70 @@ def store_item_id(ITEM_ID):
     return RESULT
 
 ###########################################################################
+## Mythic Keystone Affix API
+###########################################################################
+
+###########################################################################
+## Mythic Raid Leaderboard API
+###########################################################################
+
+###########################################################################
+## Mount API
+###########################################################################
+
+###########################################################################
+## Mythic Keystone Dungeon API
+###########################################################################
+
+###########################################################################
+## Mythic Keystone Leaderboard API
+###########################################################################
+
+###########################################################################
+## Pet API
+###########################################################################
+
+###########################################################################
+## Playable Class API
+###########################################################################
+
+###########################################################################
+## Playable Race API
+###########################################################################
+
+###########################################################################
+## Playable Specialization API
+###########################################################################
+
+###########################################################################
+## Power Type API
+###########################################################################
+
+###########################################################################
+## PvP Season API
+###########################################################################
+
+###########################################################################
+## PvP Tier API
+###########################################################################
+
+###########################################################################
+## Realm API
+###########################################################################
+
+###########################################################################
+## Region API
+###########################################################################
+
+###########################################################################
+## Title API
+###########################################################################
+
+###########################################################################
+## WoW Token API
+###########################################################################
+
+###########################################################################
 ###########################################################################
 
 def zone_master_list():
@@ -185,10 +413,7 @@ def zone_master_list():
     PARAMS = {'region':REGION, 'namespace':NAMESPACE, 'locale':LOCALE}
     FULL_URL = f"{URL}{API_HTTP_PATH}"
     RESULT = requests.get(FULL_URL, headers=AUTH_HEADER, params=PARAMS).json()
-    print(json.dumps(RESULT, indent=4, sort_keys=True))
-
+    print(json.dumps(RESULT, indent=2, sort_keys=True))
 
 ###########################################################################
 ###########################################################################
-
-achievement_index()
